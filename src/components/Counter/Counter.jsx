544a654aec473755733d5432445ae2761b3243bd
Manish Kumar <manish1771999@gmail.com>
2022-05-28 16:15:23 +0530

    RCT-101 E1

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import styles from "./counter.module.css";
 
-const Counter = () => {
-  // sample value to be replaced
-  let count = 0;
-  // NOTE: do not delete `data-cy` key value pair
+const Counter = ({ count, updateCount }) => {
   return (
     <div className={styles.counter}>
-      <button data-cy="task-counter-increment-button"></button>
+      <button
+        data-cy="task-counter-increment-button"
+        onClick={() => updateCount(count + 1)}
+      >
+        +
+      </button>
       <span data-cy="task-counter-value">{count}</span>
-      <button data-cy="task-counter-decrement-button"></button>
+      <button
+        data-cy="task-counter-decrement-button"
+        onClick={() => updateCount(count - 1)}
+      >
+        -
+      </button>
     </div>
   );
 };
--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,14 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { v4 } from "uuid";
 import styles from "./taskApp.module.css";
+import { TaskHeader } from "./TaskHeader";
+import { AddTask } from "./AddTask";
+import { Tasks } from "./Tasks";
+import taskData from "../data/tasks.json";
 
 const TaskApp = () => {
-  // NOTE: do not delete `data-cy` key value pair
+  const [tasks, setTasks] = useState(taskData);
+
+  const addTask = (newTask) => {
+    let isTaskPresent = tasks.some((task) => task.text === newTask);
+    if (newTask && !isTaskPresent) {
+      const newTaskObj = {
+        id: v4(),
+        text: newTask,
+        done: false,
+        count: 1,
+      };
+      setTasks([...tasks, newTaskObj]);
+    }
+  };
+
+  const handleUpdateTask = (updatedTask) => {
+    let newTasks = tasks.reduce((acc, curr) => {
+      if (curr.id === updatedTask.id) {
+        acc.push(updatedTask);
+      } else {
+        acc.push(curr);
+      }
+      return acc;
+    }, []);
+
+    setTasks([...newTasks]);
+  };
+
+  const handleRemoveTask = (taskId) => {
+    let newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
+
   return (
-    <div data-cy="task-app" className={styles.taskApp}>
-      {/* Header */}
-      {/* Add Task */}
-      {/* Tasks */}
+    <div data-cy="task-app" className={styles.main}>
+      <div className={styles.taskApp}>
+        <TaskHeader tasks={tasks} />
+        <AddTask addTask={addTask} />
+        <Tasks
+          tasks={tasks}
+          handleUpdateTask={handleUpdateTask}
+          handleRemoveTask={handleRemoveTask}
+        />
+      </div>
     </div>
   );
 };
